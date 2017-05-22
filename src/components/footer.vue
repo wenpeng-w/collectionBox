8464cf5d360c1364f217acfef8c393cb1bdcf5af
wenpeng-w <449202631@qq.com>
2017-05-22 23:41:30 +0800

    Add footer

---
+++ src/components/footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="footer">
+    <div class="copyright">©2017 WOMENL.CN 0618 All rights reserved</div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {}
+</script>
+
+<style lang="less" rel="stylesheet/less" scoped>
+  .footer {
+    width: 100%;
+    .copyright {
+      padding: 40px;
+      text-align: center;
+      font-size: 13px;
+      color: #cccccc;
+    }
+  }
+</style>
