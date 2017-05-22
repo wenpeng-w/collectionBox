5fc45e7cb00cf81ba5980aae2ec199b619abacf3
wenpeng-w <449202631@qq.com>
2017-05-22 23:43:05 +0800

    Add gotop

---
+++ src/components/goTop.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="go-top">
+    <a @click="goTop" href="javascript:void(0);" class="content">↑</a>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  methods: {
+    goTop () {
+//      let pH = document.documentElement.clientHeight
+      document.documentElement.scrollTop = document.body.scrollTop = 0
+    }
+  }
+}
+</script>
+
+<style lang="less" rel="stylesheet/less" scoped>
+  .go-top {
+    position: fixed;
+    right: 40px;
+    bottom: 40px;
+    .content {
+      display: block;
+      width: 40px;
+      height: 40px;
+      line-height: 40px;
+      border: 1px solid #cecece;
+      border-radius: 4px;
+      cursor: pointer;
+      font-size: 16px;
+      text-align: center;
+      color: #aeaeae;
+      &:hover {
+        background-color: #f5f5f5;
+      }
+    }
+  }
+</style>
